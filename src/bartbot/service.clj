16f842b0f07dd8cab6f5ec69360a5157b2b1e347
Daniel Y Lee <taeold@gmail.com>
2016-06-05 17:50:06 -0700

    add default 404

--- src/bartbot/service.clj
+++ src/bartbot/service.clj
@@ -3,9 +3,12 @@
     [bartbot.v1.routes :refer [v1-routes]]
     [compojure.core :refer [routes]]
     [compojure.handler :refer [api]]
+    [compojure.route :refer [not-found]]
     [ring.middleware.logger :as logger]))
 
 (def handler
   (api
-    (-> (routes v1-routes)
+    (-> (routes
+          v1-routes
+          (not-found "<h1>Page not found</h1>"))
         logger/wrap-with-logger)))
