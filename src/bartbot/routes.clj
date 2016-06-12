e49e40513853949c9736914f55a03b8fa142e7e8
Daniel Y Lee <taeold@gmail.com>
2016-06-12 00:06:40 -0700

    update config edn - fb messenger

--- src/bartbot/routes.clj
+++ src/bartbot/routes.clj
@@ -8,12 +8,12 @@
   (context "/fb" []
     (GET "/chat" {{challenge "hub.challenge"
                    verify-token "hub.verify_token"} :params}
-      (if (= (immuconf/get @config :fb :verify-token) verify-token)
+      (if (= (immuconf/get @config :fb :messenger :verify-token) verify-token)
         {:status 200 :body challenge}
         {:status 401}))
     (POST "/chat" {:as request
                    {verify-token "hub.verify_token"} :params}
-      (if (= (immuconf/get @config :fb :verify-token) verify-token)
+      (if (= (immuconf/get @config :fb :messenger :verify-token) verify-token)
         {:status 200
          :body (str request)}
         {:status 401}))))
